5c309e2c798c32bd1827e3d6fbf0310ac6e6f4c6
Dennis Akagha <d.akagha20@gmail.com>
2022-08-02 00:53:04 +0100

    Set up the database

---
+++ test/controllers/inventory_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class InventoryControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get inventory_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get inventory_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get inventory_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get inventory_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get inventory_destroy_url
+    assert_response :success
+  end
+end
