7c41b067e16bd1aeaaeb586fd4fc290c05a843b2
Dennis Akagha <d.akagha20@gmail.com>
2022-08-02 19:30:14 +0100

    fix the confirmation styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
   }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
