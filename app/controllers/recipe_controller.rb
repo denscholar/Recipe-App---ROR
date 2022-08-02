6ddf8132bedcd2a470f2b7fc346a3e0570b2840b
Aduda-Boaz <adudaboaz@gmail.com>
2022-08-02 23:56:41 +0300

    Modified and updated the recipe foods controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -13,4 +13,25 @@ class RecipeController < ApplicationController
     @user = current_user
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    @recipe.user_id = params[:user_id]
+
+    respond_to do |format|
+      if @recipe.save
+        format.html do
+          redirect_to user_recipe_path(@recipe.user, @recipe)
+        end
+      else
+        format.html { redirect_to new_user_recipe_path(@recipe.user), alert: 'Recipe could not be created.' }
+      end
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipes).permit(:name, :preperation_time, :cooking_time, :description, :public)
+  end
 end
--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,2 +1,23 @@
 class RecipeFoodsController < ApplicationController
+  def new
+    @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food.recipe = @recipe
+    if @recipe_food.save
+      redirect_to user_recipe_path(@recipe.user, @recipe)
+    else
+      flash[:error] = 'Ingredient already added.'
+    end
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :quantity)
+  end
 end
