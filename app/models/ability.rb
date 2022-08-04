9fed3b278ad58f101dc3de86568cc6b64bd7aaa1
Aduda-Boaz <adudaboaz@gmail.com>
2022-08-05 00:55:08 +0300

    Fix add recipe error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,10 @@
 class Ability
   include CanCan::Ability
-
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-
     can :read, :all
     can :add, :all
-    can :destroy, Recipe, Inventory, user_id: user.id
+    can :destroy, Recipe, user_id: user.id
+    can :destroy, Inventory, user_id: user.id
   end
 end
