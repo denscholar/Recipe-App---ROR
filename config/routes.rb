d0a72ae8e27be3c0219d00ee7253d301a454dfee
Elio Cortes <elio.cortes3000@gmail.com>
2022-08-06 01:09:22 -0500

    Fix error in recipe details

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do  
   devise_for :users 
   
-  # controllers: {
-  #   sessions: 'users/sessions',
-  #   passwords: 'users/passwords',
-  #   registrations: 'users/registrations'
-  # }
   root "pages#index"
   resources :public_recipes, only: [:index]
   resources :shopping_list, only: [:index]
 
-      resources :inventories, only: %i[index show new create destroy] do
-      resources :inventory_foods, only: %i[index show new create destroy]
-    end
-    resources :foods, only: %i[index show new create destroy]
-    # resources :users, only: %i[index show] do
-      resources :recipes, only: %i[index show new create destroy] do
-        resources :recipe_foods, only: %i[index show new create destroy]
-      end
-    # end  
-  get	'/shopping_lists/:recipe_id&:inventory_id', to: 'shopping_lists#new', as: 'shopindex'
+  resources :inventories, only: %i[index show new create destroy] do
+    resources :inventory_foods, only: %i[index show new create destroy]
+  end
   resources :foods, only: %i[index show new create destroy]
+  resources :recipes, only: %i[index show new create destroy] do
+    resources :recipe_foods, only: %i[new create]
+  end
+  resources :shopping_lists, only: %i[index]
+  get	'/shopping_lists/:recipe_id&:inventory_id', to: 'shopping_lists#new', as: 'shopnew'  
+   
 end
