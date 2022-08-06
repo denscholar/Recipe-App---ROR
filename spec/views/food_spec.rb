c215aabf624b19efdc1cb6b46d99cbdab54269af
Dennis Akagha <d.akagha20@gmail.com>
2022-08-07 00:05:27 +0100

    Update test files

--- spec/views/food_spec.rb
+++ spec/views/food_spec.rb
@@ -3,62 +3,37 @@ require 'rails_helper'
 RSpec.describe 'Food view', type: :feature do
   describe 'Food index page' do
     before(:each) do
-      @user = User.new(email: 'pepe@mail.com', password: '121212', password_confirmation: '121212')
-      @user.save!
-      visit root_path
-      sleep(5)
-      visit new_user_session_path
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      click_button 'Log in'
-
-      @food = Food.create!(name: 'Ground beef', measurement_unit: 'kg', price: 12)
-    end
-
-    scenario 'validates if Food is displayed on foods page' do
-      visit '/foods'
-      expect(page).to have_content('Foods')
-    end
-
-    scenario 'user can create new food using button' do
-      visit '/foods'
-      expect(page).to have_content('New food')
-    end
-
-    scenario 'New Food button redirects to New Food form' do
-      visit '/foods'
-      click_link 'New food'
-      sleep 1.5
-      expect(page).to have_content('Price')
-    end
-  end
-
-  describe 'New food form page' do
-    before(:each) do
       @user = User.create!(name: 'Esther Alice', email: 'estherAlice@gmail.com', password: 'password',
                            confirmed_at: Time.now)
-      @food = Food.create!(name: 'Ground beef', measurement_unit: 'kg', price: 12)
-
-      visit 'users/sign_in'
-      fill_in 'Email', with: 'estherAlice@gmail.com'
-      fill_in 'Password', with: 'password'
+      @user.save!
+      visit 'users/sign_in' # root_path
+      visit new_user_session_path
+      fill_in 'Email', with: @user.email
+      sleep(1)
+      fill_in 'Password', with: @user.password
+      sleep(1)
       click_button 'Log in'
-      visit '/foods/new'
+      @food = Food.create!(name: 'Ground beef', measurement_unit: 'kg', price: 12)
     end
-
-    scenario 'form has correct fields' do
-      expect(page).to have_content('Name')
-      expect(page).to have_content('Measurement unit')
-      expect(page).to have_content('Price')
+    scenario 'validates if Food is displayed on foods page' do
+      visit '/foods'
+      expect(page).to have_content('This is the food view')
     end
-
-    scenario 'users can create create food' do
-      fill_in 'Name', with: 'bannann'
-      fill_in 'Measurement unit', with: 'Pat'
-      fill_in 'Price', with: 100
-      click_button 'Create Food'
-
-      expect(page).to have_content('Food created succesfully')
+    scenario 'user can create new food using button' do
+      click_link 'Food'
+      expect(page).to have_content('Add Food')
+    end
+    scenario 'New Food button redirects to New Food form' do
+      click_link 'Food'
+      click_link 'Add Food'
+      sleep(1)
+      expect(page).to have_content('Add Food here')
+    end
+    scenario 'new form has correct button' do
+      click_link 'Food'
+      click_link 'Add Food'
+      expect(page).to have_content('Add Food')
+      expect(page).to have_content('Back to food list')
     end
   end
 end
